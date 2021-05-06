2de25ef4e55fd2e2fa5ff8555f0606c52e548b02
TheOnly8Z <8888888zombies@gmail.com>
2021-05-06 19:31:22 +0800

    Underload is now a fg (consistent w/ loose springs)

--- lua/arccw/shared/attachments/ud_fg_underload.lua
+++ lua/arccw/shared/attachments/ud_fg_underload.lua
@@ -1,7 +1,7 @@
 att.PrintName = "Underload"
 
 att.Icon = Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth")
-att.Description = "Just because it fits, does not mean you have to fill it. Partially filling magazines reduces follower stress, improving feed rates."
+att.Description = "Just because it fits, does not mean you have to fill it. Partially filled magazines reduces follower stress and improves feeding rate."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -9,16 +9,17 @@ att.Desc_Cons = {
 }
 att.Desc_Neutrals = {
 }
-att.Slot = "ud_training"
+att.Slot = "ud_fg"
 
 att.AutoStats = true
 
 -- I give up, arctic help pls
 
 function att.Hook_GetCapacity(wep,cap)
-    return math.max(math.floor(cap * 0.84), 1)
+    return math.max(math.floor(cap * (1 - 0.14)), 1)
 end
 
 -- att.Add_ClipSize = math.floor(att.Hook_GetCapacity() * -.25)
 att.Mult_HeatCapacity = 1.25
 att.Mult_RPM = 1.05
+att.Mult_ReloadTime = 0.95

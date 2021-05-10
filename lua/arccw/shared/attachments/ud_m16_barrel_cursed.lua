53a8e87cf3f6536348d9a08a7b64a50ed038ab62
TheOnly8Z <8888888zombies@gmail.com>
2021-05-10 20:24:39 +0800

    modify qc and add cursed barrel

---
+++ lua/arccw/shared/attachments/ud_m16_barrel_cursed.lua
@@ -0,0 +1,22 @@
+att.PrintName = "20\" Cursed Barrel"
+
+att.Icon = Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth")
+att.Description = "WHAT THE FUCK IS THIS?"
+att.Desc_Pros = {
+    "????????????"
+}
+att.Desc_Cons = {
+    "????????????"
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "ud_m16_barrel"
+
+att.AutoStats = false
+
+att.Mult_SightTime = 0.9
+att.Mult_SightedSpeedMult = 1.1
+
+att.LHIK = true
+
+att.Model = "models/weapons/arccw/atts/fpw_lhik.mdl"

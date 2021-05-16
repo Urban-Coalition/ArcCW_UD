bfca958ba33a24c57341ea8e97b2779928027d63
GamerBarney <68207899+GamerBarney@users.noreply.github.com>
2021-05-15 19:43:08 -0500

    nobody wanted this arctic

---
+++ lua/arccw/shared/attachments/ud_m16_receiver_auto.lua
@@ -0,0 +1,36 @@
+att.PrintName = "AMCAR Automatic Receiver"
+
+if GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "M16 Automatic Receiver"
+end
+
+att.Icon = Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth")
+att.Description = "An automatic receiver with auto-semi-safe firemodes, used in small quantities by the U.S. Navy."
+att.Desc_Pros = {
+    "ud.auto"
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "ud_m16_receiver"
+
+att.AutoStats = true
+
+att.Mult_AccuracyMOA = 1.25
+att.Mult_RPM = 0.9
+
+att.Override_Firemodes = {
+    {
+        Mode = 2,
+    },
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+att.GivesFlags = {"m16_auto"}
+att.ExcludeFlags = {"m16_noauto"}

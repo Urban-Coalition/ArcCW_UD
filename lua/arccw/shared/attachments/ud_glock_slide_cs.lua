719673cd071cc7638c00afa70ed1cd334c9f3e75
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2021-06-30 15:54:04 +0800

    20round cs g17, localize m16 charm

--- lua/arccw/shared/attachments/ud_glock_slide_cs.lua
+++ lua/arccw/shared/attachments/ud_glock_slide_cs.lua
@@ -6,9 +6,10 @@ if GetConVar("arccw_truenames"):GetBool() then
 end
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "3 round burst conversion, custom-made and used by various counter-terrorism units and 'agents' around the world."
+att.Description = "Custom-made 3 round burst slide, used by various counter-terrorism units and 'agents' around the world. The standard-length magazines are also tweaked for increased capacity."
 att.Desc_Pros = {
     "ud.3burst",
+    "ud.glock.cs",
 }
 att.Desc_Cons = {
     "ud.jam"
@@ -19,10 +20,7 @@ att.Slot = "ud_glock_slide"
 
 att.AutoStats = true
 
---att.Mult_SightTime = 1
---att.Mult_Recoil = 1
---att.Mult_SpeedMult = 1
-att.Mult_MoveDispersion = 1.4
+--att.Mult_MoveDispersion = 1.4
 att.Mult_HipDispersion = 1.20
 att.Mult_ShootSpeedMult = 0.85
 
@@ -32,7 +30,12 @@ att.Override_Firemodes = {
     {
         Mode = -3,
         Mult_RPM = 4,
-        PostBurstDelay = 0.15
+        PostBurstDelay = 0.25,
+        RunawayBurst = true,
+        ShotRecoilTable = {
+            [1] = 0.5,
+            [2] = 0.5,
+        }
     },
     {
         Mode = 1,
@@ -48,5 +51,24 @@ att.Override_Firemodes = {
 --         return anim .. "_18"
 --     end
 -- end
+
+--[[]
+function att.Hook_GetCapacity(wep,cap)
+    if not wep.Attachments[6].Installed then
+        return cap + 3
+    end
+end
+]]
+att.A_Hook_Add_ClipSize = function(wep, data)
+    if not wep.Attachments[6].Installed then
+        data.add = (data.add or 0) + 3
+    end
+end
+
+att.Hook_GetDefaultAttName = function(wep, slot)
+    if slot == 6 then
+        return "20-Round Mag"
+    end
+end
 
 att.ActivateElements = {"ud_glock_slide_cs"}

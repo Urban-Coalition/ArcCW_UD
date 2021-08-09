30ddff80ed420381f8397ad388f6a21f0e22c14e
Haodong Mo <mohaodong@gmail.com>
2021-08-09 15:10:51 +1000

    Service Rifle barrel no longer semi auto

--- lua/arccw/shared/attachments/ud_m16_barrel_wood.lua
+++ lua/arccw/shared/attachments/ud_m16_barrel_wood.lua
@@ -1,7 +1,7 @@
 att.PrintName = "20\" \"Service Rifle\" Barrel"
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "Standard barrel with custom wooden handguard. Such products are created only by independent gunsmiths from the state of California, and are semi-automatic only due to what seems to be personal preference."
+att.Description = "Standard barrel with custom wooden handguard. Such products are created only by independent gunsmiths from the state of California."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -17,15 +17,15 @@ att.Mult_AccuracyMOA = 0.8
 att.Mult_Sway = 0.9
 att.Mult_Recoil = 0.9
 
-att.Override_Firemodes_Priority = -1
-att.Override_Firemodes = {
-    {
-        Mode = 1,
-    },
-    {
-        Mode = 0
-    }
-}
+-- att.Override_Firemodes_Priority = -1
+-- att.Override_Firemodes = {
+--     {
+--         Mode = 1,
+--     },
+--     {
+--         Mode = 0
+--     }
+-- }
 
 att.LHIK = true
 

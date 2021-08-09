878cbe1e55b13e7573a96d320ce3107e4d3adf52
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2021-08-09 16:21:38 +0800

    tweak uzi stocks

--- lua/arccw/shared/attachments/ud_uzi_stock_polymer.lua
+++ lua/arccw/shared/attachments/ud_uzi_stock_polymer.lua
@@ -6,20 +6,22 @@ if !GetConVar("arccw_truenames"):GetBool() then
 end
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "A solid, non-foldable stock made of polymer. Old-fashioned and very sturdy, though less compact."
+att.Description = "A solid, non-foldable stock made of polymer. Provides decent recoil reduction and doesn't weigh as much as a full wooden stock."
 att.Desc_Pros = {}
 att.Desc_Cons = {}
 att.Desc_Neutrals = {}
 att.Slot = "ud_uzi_stock"
 
 att.AutoStats = true
+att.SortOrder = 3
 
-att.Mult_Recoil = 0.75
-att.Mult_RecoilSide = 0.5
-att.Mult_VisualRecoilMult = 0.5
+att.Mult_Recoil = 0.85
+att.Mult_RecoilSide = 0.75
+att.Mult_VisualRecoilMult = 0.75
 
-att.Mult_SightTime = 1.25
-att.Mult_SightedSpeedMult = 0.75
+att.Mult_SightTime = 1.15
+att.Mult_SightedSpeedMult = 0.95
+att.Mult_ShootSpeedMult = 0.95
 
 att.Mult_DrawTime = 1.25
 att.Mult_HolsterTime = 1.25

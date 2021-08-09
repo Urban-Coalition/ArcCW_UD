2de77943c2594d5bd6fc9575abe3788825f83ed9
Sninctbur <hedpub@protonmail.com>
2021-08-09 04:26:11 -0500

    I misclicked

--- lua/arccw/shared/attachments/ud_m16_barrel_wood.lua
+++ lua/arccw/shared/attachments/ud_m16_barrel_wood.lua
@@ -1,7 +1,7 @@
 att.PrintName = "20\" \"Service Rifle\" Barrel"
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "Standard barrel with custom wooden handguard. Such products are created only by independent gunsmiths from the state of California."
+att.Description = "Custom wooden handguard created by independent gunsmiths from the state of California. Its heft gives it some extra stability."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -13,9 +13,12 @@ att.Slot = "ud_m16_barrel"
 
 att.AutoStats = true
 
-att.Mult_AccuracyMOA = 0.8
-att.Mult_Sway = 1.25
-att.Mult_Recoil = 0.9
+att.Mult_Sway = 0.85
+att.Mult_RecoilSide = 0.85
+att.Mult_Recoil = 0.95
+
+att.Mult_SightedSpeedMult = 0.95
+att.Mult_ShootSpeedMult = 0.95
 
 -- att.Override_Firemodes_Priority = -1
 -- att.Override_Firemodes = {

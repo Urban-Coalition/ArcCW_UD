e7b648e7567a6caaf2c7d68431ea40789474ae3b
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2021-08-11 17:36:33 +0800

    870 poly slide

--- lua/arccw/shared/attachments/ud_870_slide_poly.lua
+++ lua/arccw/shared/attachments/ud_870_slide_poly.lua
@@ -6,7 +6,7 @@ if GetConVar("arccw_truenames"):GetBool() then
 end
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "A hardened polymer forend. Faster to cycle but harder to grip on, increasing both fire rate and recoil."
+att.Description = "A hardened polymer forend. Lighter weight improves sighted mobility at the cost of recoil."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -17,17 +17,11 @@ att.Slot = "ud_870_slide"
 
 att.AutoStats = true
 
--- att.Mult_SightTime = 0.75
--- att.Mult_Recoil = 1.15
--- att.Mult_Sway = 1.15
+att.Mult_SightTime = 0.9
+att.Mult_SightedSpeedMult = 1.1
 
-att.Mult_SightTime = 0.85
-
-att.Mult_Sway = 1.25
+att.Mult_Sway = 1.15
 att.Mult_Recoil = 1.1
-att.Mult_RecoilSide = 1.5
-
-att.Mult_CycleTime = 0.9
 
 att.LHIK = true
 

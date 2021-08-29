5da8f5bbe96b634d0caf638fe5b62d14bc5d79ca
GamerBarney <68207899+GamerBarney@users.noreply.github.com>
2021-08-29 17:50:15 -0500

    all realism and no fun makes arctic a dull boy

--- lua/arccw/shared/attachments/ud_m16_tl.lua
+++ lua/arccw/shared/attachments/ud_m16_tl.lua
@@ -1,5 +1,5 @@
 att.PrintName = "Tactical Laser Position"
-att.Icon = Material("entities/att/arccw_ud_keepitretro.png", "smooth mips")
+att.Icon = Material("entities/att/arccw_ud_pointshoot.png", "smooth mips")
 att.Description = "Give your AN/PEQ a reason to be thin."
 att.Desc_Pros = {}
 att.Desc_Cons = {}
@@ -16,7 +16,7 @@ att.Slot = "ud_m16_charm"
 att.ActivateElements = {"bravo_dicks_going_fart"}
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 25, -3.5), -- relative to where att.Model is placed
+        Pos = Vector(0, 22.5, -4), -- relative to where att.Model is placed
         Ang = Angle(0, 0, -45),
         GlobalPos = false,
         GlobalAng = true,

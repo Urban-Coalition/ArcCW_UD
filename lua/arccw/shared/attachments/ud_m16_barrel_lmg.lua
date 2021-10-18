78685ecc7e637cab51dc42fc04dbcd094b693368
TheOnly8Z <888888zombies@gmail.com>
2021-10-18 13:46:30 -0400

    buff colt LMG barrel

--- lua/arccw/shared/attachments/ud_m16_barrel_lmg.lua
+++ lua/arccw/shared/attachments/ud_m16_barrel_lmg.lua
@@ -5,7 +5,7 @@ if GetConVar("arccw_truenames"):GetBool() then
 end
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "Heavy duty handguard with built-in bipod and foregrip, sporting improved durability and heat dissipation capabilities for sustained automatic fire."
+att.Description = "Heavy duty handguard with built-in bipod and foregrip, sporting improved durability and recoil control for sustained automatic fire."
 att.Desc_Pros = {}
 att.Desc_Cons = {
     "uc.noubs"
@@ -15,13 +15,13 @@ att.Slot = "ud_m16_barrel"
 
 att.AutoStats = true
 
-att.Mult_SpeedMult = 0.925
+att.Mult_SpeedMult = 0.95
 att.Mult_SightedSpeedMult = 0.75
-att.Mult_Sway = 2
+att.Mult_Sway = 1.5
 att.Mult_SightTime = 1.25
 
 att.Mult_RPM = 0.915
-att.Mult_Recoil = 0.75
+att.Mult_Recoil = 0.7
 att.Mult_RecoilSide = 0.5
 
 --att.Mult_HeatDelayTime = 0.5
@@ -30,8 +30,8 @@ att.Mult_MalfunctionMean = 1.5
 
 att.Bipod = true
 att.Override_InBipodPos = Vector(-10, 0, -4)
-att.Mult_BipodDispersion = 0.3
-att.Mult_BipodRecoil = 0.3
+att.Mult_BipodDispersion = 0.25
+att.Mult_BipodRecoil = 0.2
 
 att.ActivateElements = {"ud_m16_barrel_lmg"}
 att.GivesFlags = {"m16_lmg"}

f1e9e162f5fd8950ead2d786dcf4a9fc2a93d6d3
TheOnly8Z <888888zombies@gmail.com>
2021-10-22 16:37:08 -0400

    cool description

--- lua/arccw/shared/attachments/ud_m16_strap_bo1.lua
+++ lua/arccw/shared/attachments/ud_m16_strap_bo1.lua
@@ -1,12 +1,13 @@
 att.PrintName = "MACV-SOG Wrapped Sling"
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "insert cool descriptoin"
+att.Description = "Wrap your sling around the weapon to make it look cooler and more commando-like."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
 }
 att.Desc_Neutrals = {
+    "uc.cosmetic"
 }
 att.Slot = "ud_m16_charm"
 

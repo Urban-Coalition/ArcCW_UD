0339b5e0a4f68781a2878bf5f419ea4f96b38171
TheOnly8Z <888888zombies@gmail.com>
2021-10-27 15:59:03 -0400

    tweak stats of m16 semi

--- lua/arccw/shared/attachments/ud_m16_receiver_semi.lua
+++ lua/arccw/shared/attachments/ud_m16_receiver_semi.lua
@@ -5,13 +5,13 @@ if GetConVar("arccw_truenames"):GetBool() then
     att.PrintName = "AR-15 Sporting Receiver"
 end
 
-att.Description = "A semi-automatic only receiver on civilian markets. Designed for sport shooting, this receiver is more accurate and lightweight."
+att.Description = "A semi-automatic receiver sold in civilian markets. Designed for sport shooting, this receiver is more accurate and lightweight."
 
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
-    "uc.semi"
+    "uc.semionly"
 }
 att.Desc_Neutrals = {
 }
@@ -19,10 +19,13 @@ att.Slot = "ud_m16_receiver"
 
 att.AutoStats = true
 
-att.Mult_AccuracyMOA = 0.6
-att.Mult_SightTime = 0.85
+att.Mult_RPM = 600 / 900
+att.Mult_Recoil = 0.9
+att.Mult_AccuracyMOA = 0.5
+att.Mult_Range = 1.25
 att.Mult_MoveDispersion = 0.75
 
+att.Override_Firemodes_Priority = 0.5
 att.Override_Firemodes = {
     {
         Mode = 1,

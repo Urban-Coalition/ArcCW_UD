e19196308e277a93e08adc0fce7333581fc3e968
rzen1th <61303091+rzen1th@users.noreply.github.com>
2021-10-26 20:53:15 -0500

    mini 14 stuff

--- lua/arccw/shared/attachments/ud_mini14_mag_30_762.lua
+++ lua/arccw/shared/attachments/ud_mini14_mag_30_762.lua
@@ -1,13 +1,13 @@
-att.PrintName = "Mini-14 30-Round 7.62 Mag"
+att.PrintName = "Mini-30 7.62x39mm Magazine"
 att.AbbrevName = "30-Round 7.62 Mag"
 
 if !GetConVar("arccw_truenames"):GetBool() then
-    att.PrintName = "Patriot 809 30-Round 7.62 Mag"
+    att.PrintName = "Patriot 816 30-Round 7.62x39mm Mag"
 end
 
 att.SortOrder = 30
 att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "Zenith eats shit."
+att.Description = "You should kill yourself NOW!"
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -25,6 +25,14 @@ att.Mult_Sway = 1.5
 att.Mult_ShootSpeedMult = 0.95
 
 att.ActivateElements = {"ud_mini14_30_mag"}
+
+att.Hook_GetShootSound = function(wep, sound)
+    if wep:GetBuff_Override("Silencer") then
+        return "weapons/arccw_ud/mini14/fire_762_supp.ogg" -- Not Placeholder
+    else
+        return "weapons/arccw_ud/mini14/fire_762.ogg" -- Not Placeholder
+    end
+end
 
 att.Hook_SelectReloadAnimation = function(wep, anim)
     return anim .. "_30"

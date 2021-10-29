0e9cb5a53b0f6fd8be8201f8c01309163b28f3ec
TheOnly8Z <888888zombies@gmail.com>
2021-10-29 15:13:45 -0400

    shit fuck fuck shit

---
+++ lua/arccw/shared/sh_ud.lua
@@ -0,0 +1,8 @@
+ArcCW.UD = ArcCW.UD or {}
+
+ArcCW.UD.ADSReload = function(wep)
+    local vm = wep:GetOwner():GetViewModel()
+    local delta = wep:GetSightDelta()
+    local coolilove = math.cos(delta * (math.pi / 2))
+    vm:SetPoseParameter( "sights", Lerp(coolilove, 0, 1) ) -- thanks fesiug
+end

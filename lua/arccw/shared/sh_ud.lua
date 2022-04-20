1907d20d8e23c6846028dbdc52fd9b53df88a67f
Fesiug <publicfesiug@outlook.com>
2022-04-19 23:56:19 -0400

    Next person to commit definitely likes men

--- lua/arccw/shared/sh_ud.lua
+++ lua/arccw/shared/sh_ud.lua
@@ -33,49 +33,52 @@ local tracebase = {
 }
 ArcCW.UD.InnyOuty = function(wep)
     if (game.SinglePlayer() and SERVER) or (!game.SinglePlayer() and CLIENT and wep:GetOwner() == LocalPlayer()) then
-        local vol = 0
-        local wo = wep:GetOwner()
-        local wop = wo:EyePos()
-        local woa = Angle(0, wo:EyeAngles().y, 0)
-        local t_influ = 0
+        if wep.DistantShootSoundOutdoors and wep.DistantShootSoundOutdoors then
 
-        for _, tin in ipairs(traces) do
-            tracebase.start = wop
-            offset = Vector()
-            if _ != 1 then
-                offset = offset + (tin.Distance.x * woa:Right())
-                offset = offset + (tin.Distance.y * woa:Forward())
-                -- No-one shoots Zs. -- offset = offset + (tin.Distance.z * woa:Up())
-            else -- Specific skytrace
-                offset = offset + (vector_up*1024)
-            end
-            tracebase.endpos = wop + offset
-            tracebase.filter = wo
-            t_influ = t_influ + (tin.Influence or 1)
-            local result = util.TraceLine(tracebase)
-            debugoverlay.Line(wop-(vector_up*4), result.HitPos-(vector_up*4), 1, Color((_/4)*255, 0, (1-(_/4))*255))
-            debugoverlay.Text(result.HitPos-(vector_up*4), math.Round((result.HitSky and 1 or result.Fraction)*100).."%", 1)
-            vol = vol + (result.HitSky and 1 or result.Fraction) * tin.Influence
-        end
+            local vol = 0
+            local wo = wep:GetOwner()
+            local wop = wo:EyePos()
+            local woa = Angle(0, wo:EyeAngles().y, 0)
+            local t_influ = 0
 
-        vol = vol / t_influ
-        if wep.DistantShootSoundOutdoors then
-            for _, snd in ipairs(wep.DistantShootSoundOutdoors) do
-                wep:StopSound(snd)
+            for _, tin in ipairs(traces) do
+                tracebase.start = wop
+                offset = Vector()
+                if _ != 1 then
+                    offset = offset + (tin.Distance.x * woa:Right())
+                    offset = offset + (tin.Distance.y * woa:Forward())
+                    -- No-one shoots Zs. -- offset = offset + (tin.Distance.z * woa:Up())
+                else -- Specific skytrace
+                    offset = offset + (vector_up*1024)
+                end
+                tracebase.endpos = wop + offset
+                tracebase.filter = wo
+                t_influ = t_influ + (tin.Influence or 1)
+                local result = util.TraceLine(tracebase)
+                debugoverlay.Line(wop-(vector_up*4), result.HitPos-(vector_up*4), 1, Color((_/4)*255, 0, (1-(_/4))*255))
+                debugoverlay.Text(result.HitPos-(vector_up*4), math.Round((result.HitSky and 1 or result.Fraction)*100).."%", 1)
+                vol = vol + (result.HitSky and 1 or result.Fraction) * tin.Influence
             end
-            if math.max(0.15, vol) != 0.15 then
-                wep:EmitSound(wep.DistantShootSoundOutdoors[math.random(1, #wep.DistantShootSoundOutdoors)], 75, 100, (vol) * wep.DistantShootSoundOutdoorsVolume or 1, CHAN_AUTO)
-            end
-        end
-        if wep.DistantShootSoundIndoors then
-            for _, snd in ipairs(wep.DistantShootSoundIndoors) do
-                wep:StopSound(snd)
-            end
-            if math.min(0.85, vol) != 0.85 then
-                wep:EmitSound(wep.DistantShootSoundIndoors[math.random(1, #wep.DistantShootSoundIndoors)], 75, 100, (1-vol) * wep.DistantShootSoundIndoorsVolume or 1, CHAN_AUTO)
-            end
-        end
 
+            vol = vol / t_influ
+            if wep.DistantShootSoundOutdoors then
+                for _, snd in ipairs(wep.DistantShootSoundOutdoors) do
+                    wep:StopSound(snd)
+                end
+                if math.max(0.15, vol) != 0.15 then
+                    wep:EmitSound(wep.DistantShootSoundOutdoors[math.random(1, #wep.DistantShootSoundOutdoors)], 75, 100, (vol) * wep.DistantShootSoundOutdoorsVolume or 1, CHAN_AUTO)
+                end
+            end
+            if wep.DistantShootSoundIndoors then
+                for _, snd in ipairs(wep.DistantShootSoundIndoors) do
+                    wep:StopSound(snd)
+                end
+                if math.min(0.85, vol) != 0.85 then
+                    wep:EmitSound(wep.DistantShootSoundIndoors[math.random(1, #wep.DistantShootSoundIndoors)], 75, 100, (1-vol) * wep.DistantShootSoundIndoorsVolume or 1, CHAN_AUTO)
+                end
+            end
+
+        end
     end
 end
 

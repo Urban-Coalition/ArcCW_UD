e96c104b383a8d0225064e3b5c70c9a08c17709f
Sninctbur <hedpub@protonmail.com>
2022-05-23 12:25:55 -0500

    Retro mounts moved to Front Sight slot

--- lua/arccw/shared/attachments/ud_m16_charm_ch.lua
+++ lua/arccw/shared/attachments/ud_m16_charm_ch.lua
@@ -8,7 +8,7 @@ att.Desc_Neutrals = {
     "ud.m16_ch.2",
     "ud.m16_nors"
 }
-att.Slot = "ud_m16_charm"
+att.Slot = "ud_m16_fs"
 
 att.ActivateElements = {"ud_m16_upper_charm"}
 att.GivesFlags = {"ud_m16_retro"}

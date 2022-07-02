6c3f4c75babbcac7ef7f40542102205478655f15
TheOnly8Z <888888zombies@gmail.com>
2022-07-02 13:26:50 -0500

    custom skin text

--- lua/arccw/shared/attachments/ud_glock_skin_custom.lua
+++ lua/arccw/shared/attachments/ud_glock_skin_custom.lua
@@ -1,5 +1,12 @@
-att.PrintName = "Custom"
+att.PrintName = "Glock 17 Custom Finish"
+att.Description = "Make your Glock truly yours by giving it a horrible color nobody else wants to touch."
+att.AbbrevName = "Custom"
+
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "GEN3 Custom Finish"
+    att.Description = "Make your gun truly yours by giving it a horrible color nobody else wants to touch."
+end
+
 att.Icon = Material("entities/att/acwatt_ud_glock_material.png", "smooth mips")
-att.Description = "so real"
-att.Desc_Neutrals = {"uc.cosmetic"}
+att.Desc_Neutrals = {"uc.cosmetic", "uc.custcolor"}
 att.Slot = "ud_glock_skin"
--- lua/arccw/shared/attachments/ud_glock_skin_olive.lua
+++ lua/arccw/shared/attachments/ud_glock_skin_olive.lua
@@ -1,4 +1,9 @@
-att.PrintName = "Olive Drab"
+att.PrintName = "Glock 17 Olive Drab Finish"
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "GEN3 Olive Drab Finish"
+end
+att.AbbrevName = "Olive Drab"
+
 att.Icon = Material("entities/att/acwatt_ud_glock_material.png", "smooth mips")
 att.Description = "Olive drab finish for for your polymer handgun."
 att.Desc_Neutrals = {"uc.cosmetic"}

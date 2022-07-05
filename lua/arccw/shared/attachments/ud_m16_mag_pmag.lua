7f17a062e583f01a02a7c8800028f878cbc8aec2
1darky <95997959+1darky@users.noreply.github.com>
2022-07-06 02:02:36 +0700

    :parking: + sd size + sd unscope

---
+++ lua/arccw/shared/attachments/ud_m16_mag_pmag.lua
@@ -0,0 +1,27 @@
+att.PrintName = "STANAG 30-Round Magpul PMAG"
+att.AbbrevName = "30-Round PMAG"
+att.SortOrder = 40
+att.Icon = Material("entities/att/acwatt_ud_m16_mag_40.png", "smooth mips")
+att.Description = "owo"
+att.Desc_Pros = {}
+att.Desc_Cons = {}
+att.Desc_Neutrals = {}
+att.Slot = "ud_m16_mag"
+
+att.AutoStats = true
+att.HideIfBlocked = true
+
+-- att.Mult_SightTime = 1.1
+-- att.Mult_ReloadTime = 1.15
+-- att.Mult_Sway = 1.5
+
+-- att.Mult_SpeedMult = 0.975
+-- att.Mult_ShootSpeedMult = 0.95
+
+att.ActivateElements = {"ud_m16_pmag"}
+
+att.Hook_SelectReloadAnimation = function(wep, anim) -- this is so red mountian man can make plasticc soudn
+    return anim .. "_pmag"
+end
+
+att.ExcludeFlags = {"m16_usas", "m16_9mm", "m16_50beo"}

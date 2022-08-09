ce847c921f72e6ff7fceee5ce7c1fe48a472bbd5
TheOnly8Z <888888zombies@gmail.com>
2022-08-09 01:12:24 -0500

    rebalance heatshield and fix desc

--- lua/arccw/shared/attachments/ud_m16_hg_heat.lua
+++ lua/arccw/shared/attachments/ud_m16_hg_heat.lua
@@ -1,10 +1,10 @@
 att.AbbrevName = "Heatshield Handguard"
 
 att.PrintName = "Heatshield Handguard"
-att.Description = "Heatshield handguard used on grenadier variants of the M16."
-if GetConVar("arccw_truenames"):GetBool() then
+att.Description = "Heatshield handguard used on grenadier variants of the M16.\nIts light weight offsets the downsides of an attached grenade launcher somewhat."
+if !GetConVar("arccw_truenames"):GetBool() then
     att.PrintName = "M16A1 Heatshield Handguard"
-	att.Description = "Heatshield handguard used on grenadier variants of the RAYCAR."
+    att.Description = "Heatshield handguard used on grenadier variants of the RAYCAR.\nIts light weight offsets the downsides of an attached grenade launcher somewhat."
 end
 att.Slot = "ud_m16_hg"
 att.AutoStats = true
@@ -12,7 +12,9 @@ att.AutoStats = true
 att.Mult_RecoilSide = 1.35
 
 att.Mult_SightTime = 0.9
-att.Mult_HipDispersion = 0.9
+att.Mult_HeatDissipation = 2
+att.Mult_SightedSpeedMult = 1.1
+-- att.Mult_HipDispersion = 0.9
 
 att.LHIK = false
 att.Model = "models/weapons/arccw/atts/classic_lhik.mdl"

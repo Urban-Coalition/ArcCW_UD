2c7b6af44a646afc047fce39bd106d98a2ee4812
rzen1th <pierce4077m@gmail.com>
2022-09-11 22:20:52 -0500

    far cry

--- lua/arccw/shared/attachments/ud_870_stock_sawnoff.lua
+++ lua/arccw/shared/attachments/ud_870_stock_sawnoff.lua
@@ -34,6 +34,6 @@ att.Mult_Sway = 3
 
 att.ActivateElements = {"ud_870_stock_sawnoff"}
 
-att.Override_ActivePos = Vector(0.5, 5, 0.5)
+att.Override_ActivePos = Vector(0, 2.5, -0.5)
 att.Override_HoldtypeActive = "shotgun"
 att.Override_HoldtypeSights = "ar2"

19774f5546daed58a25e2884c91babb6fa12459e
Sninctbur <hedpub@protonmail.com>
2022-10-04 11:51:14 -0400

    M16 stuff

--- lua/arccw/shared/attachments/ud_m16_stock_moe.lua
+++ lua/arccw/shared/attachments/ud_m16_stock_moe.lua
@@ -27,3 +27,19 @@ att.Mult_Sway = 1.25
 att.Mult_SightedSpeedMult = 1.15
 
 att.ActivateElements = {"stock_231_tube"}
+
+att.ToggleStats = {
+    {
+        PrintName = "Extended",
+        AutoStats = true,
+    },
+    {
+        PrintName = "Collapsed",
+        AutoStats = true,
+        Mult_RecoilSide = 1.5,
+        Add_BarrelLength = -4,
+        Mult_ShootSpeedMult = 1.1,
+        Mult_SightTime = 0.9,
+        Override_ActivePos = Vector(0.33, -3, 1.33),
+    },
+}

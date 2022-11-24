129cd5db3d08defb8c114949fbcdd8b093cc189e
rzen1th <pierce4077m@gmail.com>
2022-11-23 23:15:54 -0600

    GLOCK soudns over  haul

--- lua/weapons/arccw_ud_glock.lua
+++ lua/weapons/arccw_ud_glock.lua
@@ -610,7 +610,7 @@ SWEP.Animations = {
         Source = "fire",
         Time = 16 / 30,
         ShellEjectAt = 0.03,
-        SoundTable = {{ s = {path .. "mech-01.ogg", path .. "mech-02.ogg", path .. "mech-03.ogg", path .. "mech-04.ogg", path .. "mech-05.ogg", path .. "mech-06.ogg"}, t = 0, v = 0.25 }},
+        SoundTable = {{ s = {path .. "mech-01.ogg", path .. "mech-02.ogg", path .. "mech-03.ogg", path .. "mech-04.ogg", path .. "mech-05.ogg", path .. "mech-06.ogg"}, t = 0, v = 0.5 }},
     },
     ["fire_iron"] = {
         Source = "fire",

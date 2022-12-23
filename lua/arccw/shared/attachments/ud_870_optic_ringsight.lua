40884d2b41d9c7148767a0d3c669411c930a0e62
Fesiug <publicfesiug@outlook.com>
2022-12-23 18:53:32 -0500

    Update ud_870_optic_ringsight.lua

--- lua/arccw/shared/attachments/ud_870_optic_ringsight.lua
+++ lua/arccw/shared/attachments/ud_870_optic_ringsight.lua
@@ -1,4 +1,5 @@
 att.PrintName = "Ring Sight"
+att.Icon = Material("entities/att/acwatt_ud_870_optic_ringsight.png", "mips smooth")
 att.Description = "Rear sight module that allows for easier iron sight alignment but a reduced field of vision."
 att.Slot = "ud_870_optic"
 

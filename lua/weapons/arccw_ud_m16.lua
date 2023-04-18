2e11e4521b2c4ac7f7788c6af85e02300192e116
Fesiug <publicfesiug@outlook.com>
2023-04-18 02:28:14 -0400

    Minor unpublished updates

--- lua/weapons/arccw_ud_m16.lua
+++ lua/weapons/arccw_ud_m16.lua
@@ -160,7 +160,7 @@ SWEP.HeatDissipation = 10
 SWEP.HeatDelayTime = 3
 
 SWEP.MalfunctionMean = 200
-SWEP.MalfunctionTakeRound = false
+SWEP.MalfunctionTakeRound = true
 
 -- Speed multipliers --
 
@@ -875,7 +875,7 @@ SWEP.Animations = {
     ["fix"] = {
         Source = "fix",
         Time = 45 / 30,
-        ShellEjectAt = false,
+        ShellEjectAt = 0.3,
         LHIK = true,
         LHIKIn = 0.3,
         LHIKEaseIn = 0.4,
